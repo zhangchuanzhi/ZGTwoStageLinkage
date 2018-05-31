febe67f4c20ce2111692553fc69cc69c126323e0
zhangzhi0903 <18310127735@163.com>
2018-05-31 16:26:24 +0800

    Initial Commit

---
+++ ZGTwoStageLinkage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZGTwoStageLinkage
+//
+//  Created by offcn_zcz32036 on 2018/5/31.
+//  Copyright © 2018年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZGTwoStageLinkage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZGTwoStageLinkage
+//
+//  Created by offcn_zcz32036 on 2018/5/31.
+//  Copyright © 2018年 cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
